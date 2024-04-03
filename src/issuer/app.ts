617a7dd4c438c27fa07815709a5dfc9f2cc7d283
yeosiowvic <victoryeo1974@gmail.com>
2024-04-03 10:03:16 +0800

    convert require to import

--- src/issuer/app.ts
+++ src/issuer/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import { defaultRouter } from '../AuthRoute';
 import passport from 'passport';
-require('../Passport');
+import '../Passport';
 
 import {
   runIssuer,
